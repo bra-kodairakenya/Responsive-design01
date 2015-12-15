0d05d16f76aa8395d207612e6c8ded60b789968b
kenyakodaira <kenyanke.1@gmail.com>
2015-12-15 15:28:20 +0900

    File add and delete

--- js/min/navbar-min.js
+++
@@ -1,1 +0,0 @@
-$(function(){$(".menuButton").click(function(){return $(".menu").slideToggle(500),!1}),$(window).resize(function(){var n=$(window).width(),e=860;n>e?$(".menu").show():$(".menu").hide()})});
