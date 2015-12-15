37fbaf5cbb273930415f874edb38de6d2c21ab89
Braian <kenyanke.1@gmail.com>
2015-12-15 13:39:07 +0900

    mobile page create

---
+++ js/navbar.js
@@ -0,0 +1,17 @@
+// windowが860px以上だとメニューを表示。
+$(function(){
+  $(".menuButton").click(function(){
+    $(".menu").slideToggle(500);
+    return false;
+  });
+
+  $(window).resize(function(){
+    var win = $(window).width();
+    var size = 860;
+    if(win > size){
+      $(".menu").show();
+    } else {
+      $(".menu").hide();
+    }
+  });
+});
